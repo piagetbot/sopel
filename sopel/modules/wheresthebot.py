11709cfad97a4fcefdc435bedc16ea26c588d55d
tom29739 <tom29739@users.noreply.github.com>
2016-09-12 19:29:39 +0000

    Use print() instead of stderr()

--- sopel/modules/wheresthebot.py
+++ sopel/modules/wheresthebot.py
@@ -1,7 +1,6 @@
 from sopel.module import commands
-from sopel.tools import stderr
 
 @commands('whereisthebot', 'where', 'whereami')
 def whereisthebot(bot, trigger):
     bot.say('Hello, I\'m ' + bot.nick + '. This channel is: ' trigger.sender + '. Thanks for asking.')
-    stderr('Command: "whereisthebot" ran by ' + trigger.nick + '.')
+    print('Command: "whereisthebot" ran by ' + trigger.nick + '.')
