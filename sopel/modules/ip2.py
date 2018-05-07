b99c3a149dfedc59ba8cb24f396cfeabca0006b9
tom29739 <tom29739@users.noreply.github.com>
2018-05-07 21:35:29 +0000

    Fix ip2 module to use supported proxy detection API

--- sopel/modules/ip2.py
+++ sopel/modules/ip2.py
@@ -31,7 +31,7 @@ def ip2(bot, trigger):
         if not re.match('^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9]['
                         'a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$', query):
             return bot.say('Invalid IP address/hostname (if a hostname, try resolving it to an IP first).')
-    r = requests.get('http://ip-api.com/json/' + query + '?fields=187072')
+    r = requests.get('http://ip-api.com/json/' + query + '?fields=56000')
     if r.json()['status'] != 'success':
         return bot.say('Unknown error: ' + r.json()['message'])
     response = "[IP/Host Lookup] "
@@ -43,12 +43,15 @@ def ip2(bot, trigger):
     if r.json()['as']:
         asname = ' (AS Name: ' + r.json()['as'] + ')'
         response += asname
-    r2 = requests.get('http://legacy.iphub.info/api.php?ip=' + query + '&showtype=4&email=tom29739@tools.wmflabs.org')
-    if r2.json()['proxy']:
+    r2 = requests.get('http://v2.api.iphub.info/ip/' + query + '?key=MjA4MDpaRFh2djJSZ1B6cUpQWGZDMXc5Nm5YbUpCcmw4dndpMA==')
+    if r2.json()['block']:
         proxy = 'YES'
         response += " | Possible proxy/hosting provider: %s" % proxy
-    else:
+    else if not r2.json()['block']:
         proxy = 'NO'
+        response += " | Possible proxy/hosting provider: %s" % proxy
+    else:
+        proxy = 'POSSIBLY'
         response += " | Possible proxy/hosting provider: %s" % proxy
     coords = str(r.json()['lat']) + ', ' + str(r.json()['lon'])
     response += " | Coordinates: %s" % coords
@@ -61,14 +64,14 @@ def ip2(bot, trigger):
         r4 = requests.get('https://en.wikipedia.org/w/api.php?action=query&list=blocks&format=json&bkip=' + query)
         blockid = r4.json()['query']['blocks'][0]['id']
         response += " | Blocked on enwiki: YES (block ID: %s)" % blockid
-    except ValueError:
+    except (ValueError, IndexError):
         try:
             r4 = requests.get(
                 'https://en.wikipedia.org/w/api.php?action=query&list=globalblocks&format=json&bgip=' + query
             )
             globalblockid = r4.json()['query']['globalblocks'][0]['id']
             response += "| Blocked globally: YES (global block ID: %s)" % globalblockid
-        except ValueError:
+        except (ValueError, IndexError):
             response += " | Blocked on enwiki or globally: NO"
     finally:
         bot.say(response)
